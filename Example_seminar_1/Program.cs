0e2658e0bb62756c36ffafd969b1689f5cba91ae
melonanna <anna2510@mail.ru>
2022-12-05 14:21:16 +0300

    first example done

--- Example_seminar_1/Program.cs
+++ Example_seminar_1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a=5;
+int b=7;
+
+//Console.Write("Ответ:");
+//Console.WriteLine(a+b);
+//Console.WriteLine("спасибо за внимание");
+
+Console.Write($"ответ: {a}+{b} = {a+b}\n end of program");
+
+
