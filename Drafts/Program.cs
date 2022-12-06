8c2e5e2b72201795fb01da8fa8af519f8543abac
melonanna <anna2510@mail.ru>
2022-12-06 14:41:21 +0300

    task 3 done

---
+++ Drafts/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int N;
+Console.Write($"Введите число: ");
+
+int.TryParse(Console.ReadLine()!, out N);
+Console.Write($"Является ли число четным? ");
+
+if (N%2==0)
+{
+  Console.Write($"Да");
+}
+
+else
+{
+  Console.Write($"Нет");
+}
