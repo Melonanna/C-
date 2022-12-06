a3315f801bce4175d26068164a9572f55901c73e
melonanna <anna2510@mail.ru>
2022-12-06 14:15:54 +0300

    added seminar 2

---
+++ Example_seminar_2/Program.cs
@@ -0,0 +1,17 @@
+﻿int N;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
+
+//int n= -N;
+
+
+for (int n=-N; n<=N; n++)
+{
+  Console.Write($" {n} ");
+}
+//while (n<=N)
+// {
+ // Console.Write($" {n} ");
+ // n++;
+
+ //}
