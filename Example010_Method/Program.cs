529668ed2c59bdcaae3b8da1263cc3ad708b2efd
melonanna <anna2510@mail.ru>
2022-12-07 14:16:52 +0300

    added method

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,18 @@
+﻿int []array ={1,12,18,4,15,16,17,18};
+
+int n=array.Length;
+int find=18;
+int index=0;
+
+
+
+while (index<n)
+{
+if (array[index]==find)
+{Console.Write(index);
+break;
+
+}
+index++;
+
+} 
