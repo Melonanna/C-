f622ad7f69b9f29426daa541c16b26fb6a09bf16
melonanna <anna2510@mail.ru>
2022-12-08 13:23:42 +0300

    task from seminar3

--- Drafts/Program.cs
+++ Drafts/Program.cs
@@ -16,7 +16,7 @@ else
 }*/
 
 /*Трехзначные числа,программа показывает последнюю цифру этого числа*/
-
+/*
 int a;
 int b;
 int c;
@@ -39,3 +39,45 @@ Console.Write ("max = ");
 Console.WriteLine (max);
 
 
+*/
+
+
+ /*координаты точки x y причем x!=0, y!=0 и выдает номер плоскости где точка находится
+
+
+
+int x,y;
+
+Console.WriteLine($"Введите координату x:");
+int.TryParse(Console.ReadLine()!, out x);
+
+Console.WriteLine($"Введите координату y:");
+int.TryParse(Console.ReadLine()!, out y);
+
+if (x>0 && y>0)
+{ Console.Write($"Точка находится в 1 плоскости ");
+}
+
+else if (x>0 && y<0)
+{ Console.Write($"Точка находится в 4 плоскости ");
+}
+
+else if (x<0 && y>0)
+{ Console.Write($"Точка находится в 2 плоскости ");
+}
+
+else if (x<0 && y<0)
+{ Console.Write($"Точка находится в 3 плоскости ");
+}
+
+else 
+{Console.Write($"неправильный ввод ");
+}
+ */
+
+
+
+
+
+
+ 
