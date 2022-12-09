870aed9d6d928832c3d6714a20fd6055d27e8f35
melonanna <anna2510@mail.ru>
2022-12-09 11:39:50 +0300

    2 examples from lecture

--- Example010_Method/Program.cs
+++ Example010_Method/Program.cs
@@ -1,4 +1,4 @@
-﻿int []array ={1,12,18,4,15,16,17,18};
+﻿/*int []array ={1,12,18,4,15,16,17,18};
 
 int n=array.Length;
 int find=18;
@@ -16,3 +16,43 @@ break;
 index++;
 
 } 
+
+*/
+
+/* LECTURE 2 ON METHODS*/
+// 1 method
+
+/*void Method1()
+{
+  Console.WriteLine("Автор - Анна С");
+}
+
+Method1(); таким образом вызываем метод*/
+
+//2 method
+
+/*void Method2(string msg)
+{
+  Console.WriteLine(msg);
+}
+Method2(msg:"Текст сообщения");
+*/
+//2.1 method
+
+void Method21(string msg, int count)
+{
+int i=0;
+  while (i<count)
+{Console.WriteLine(msg);
+i++;
+}
+}
+Method21("Текст сообщения",5);
+
+
+
+
+
+
+
+
