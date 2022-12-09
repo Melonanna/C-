02584143349141b18ad7b165bdea2c227ac97ec9
melonanna <anna2510@mail.ru>
2022-12-09 19:43:04 +0300

    boring lecture with not working codes

--- Example010_Method/Program.cs
+++ Example010_Method/Program.cs
@@ -115,16 +115,74 @@ Console.WriteLine();
 
 */
 
+//string s = "qwerty"
+//            012
+// s[3] // r
+/*
+
+string Replace(string.text, char oldValue, char newValue)
+
+{
+string result=String.Empty;
+int length=text.length;
+for (int i=0; i<length;i++)
+{
+  if(text[i]==oldValue)
+   result == result+$"{newValue};
+   else result== result + ${text[i]};
+}
+return result;
+}
+
+string newText=Replace(text,' ','|');
+Console.WriteLine(newText);
+newText =Replace(newText,'к','К');
+Console.WriteLine(newText);
+
+*/
 
 
+//SORTING
+
+int [] arr = {1,5,4,3,2,6,7,1,1};
+
+void PrintArray(int[]array)
+
+{
+int count = array.Length;
+
+for (int i=0; i<count; i++)
+
+{
+  Console.Write($"{array[i]} ");
+}
+
+Console.WriteLine();
+}
+
+void SelectionSort(int[]array)
+{
+for (int i=0; i<array.Length-1; i++)
+{
+int minPosition=i;
+
+//looking for max element
+for (int j=i+1; j<array.length; j++)
+
+{
+  if(array[j]<array[minPosition]) 
+  {minPosition=j;
+  }
+}
+
+//making swap
+int temporary = array[i];
+array[i] = array [minPosition];
+array [minPosition] = temporary;
 
 
-
-
-
-
-
-
-
-
+}
+}
+//PrintArray(arr);
+//SelectionSort(arr);
 
