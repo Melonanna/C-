2ded77a86e790a1c77bc5f73df22edc4c503f3e9
melonanna <anna2510@mail.ru>
2022-12-09 12:11:11 +0300

    таблица умножения

--- Example010_Method/Program.cs
+++ Example010_Method/Program.cs
@@ -66,7 +66,7 @@ Console.WriteLine(year);
 */
 
 // 4 method
-
+/*
 string Method4(int count, string text)
 
 {
@@ -83,6 +83,42 @@ return result;
 }
 string res=Method4(10,"asdf ");
 Console.WriteLine(res);
+*/
+
+// Method 4.1
+/*
+string Method4(int count, string text)
+{
+string result = String.Empty;
+
+for (int i=0; i<count; i++)
+{
+result=result+text;
+ }
+return result;
+}
+string res=Method4(10,"asdf ");
+Console.WriteLine(res);
+
+*/
+//таблица умножения
+/*
+for (int i=2; i<=10; i++)
+{
+  for (int j=2; j<=10; j++)
+{
+Console.WriteLine($"{i}*{j}={i*j}");
+
+}
+Console.WriteLine();
+}
+
+*/
+
+
+
+
+
 
 
 
