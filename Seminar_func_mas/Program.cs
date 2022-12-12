ec54191b235b42f8929814054d28f18f0dd3c4c6
melonanna <anna2510@mail.ru>
2022-12-12 13:40:43 +0300

    added example on кол-во цифр в числе

---
+++ Seminar_func_mas/Program.cs
@@ -0,0 +1,18 @@
+﻿//НАЙТИ КОЛ-ВО ЦИФР В ЧИСЛЕ
+
+ int a= new Random().Next(1000);
+
+ int DigitsCount (int _a)
+ {
+  int i=0;
+while (_a>0)
+{
+_a=_a/10;
+i++;
+
+}
+
+return i;
+ }
+
+Console.Write($"digits in {a} = {DigitsCount(a)}");
