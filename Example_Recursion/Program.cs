a2f7eb0bc77e2be42ef2462f4e3525faf7b7b945
melonanna <anna2510@mail.ru>
2022-12-12 16:14:12 +0300

    added

--- Example_Recursion/Program.cs
+++ Example_Recursion/Program.cs
@@ -19,6 +19,7 @@ Console.WriteLine();
 
 //int [,] matrix = new int [3,4];
 
+/*
 void PrintArray(int [,] matr)
 {
 for (int i=0; i<matr.GetLength(0) ; i++)
@@ -51,22 +52,56 @@ FillArray(matrix);
 Console.WriteLine();
 PrintArray(matrix);
 
+*/
 
+/*code not working!
 
-
-/*
 void PrintImage(int [,] image)
 {
-for (int i=0; i<image.GetLength(0); i++)
+  for (int i=0; i<image.GetLength(0); i++)
 {
-for (int j=0; j<image.GetLength(0); i++)
+    for (int j=0; j<image.GetLength(1); j++)
+{
+      if (image[i,j]==0)
+Console.Write($" ");
+      else 
+Console.Write($"+");
+}
+Console.WriteLine();
+}
+}
+
+void FillImage(int row,int col);
+
+{
+if (pic[row, col] == 0)
+{
+pic[row,col]=1;
+FillImage(row-1,col);
+FillImage(row,col-1);
+FillImage(row+1,col);
+FillImage(row,col+1);
 
 }
 
-
-
-
-
 }
 
-*/
+PrintImage(pic);
+FillImage(13, 13);
+PrintImage(pic);
+ CODE NOT WORKING*/
+
+//FACTORIAL SEARCH
+
+double Factorial(int n)
+{
+if(n==1) 
+return 1;
+else 
+return n*Factorial(n-1);
+
+}
+for (int i=1; i<40; i++ )
+
+{Console.WriteLine($"{i}!= {Factorial(i)}");
+}
