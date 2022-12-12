e470191e1115ce885dda3039bb5dc977d591e0bb
melonanna <anna2510@mail.ru>
2022-12-12 18:49:52 +0300

    added

--- Example_Recursion/Program.cs
+++ Example_Recursion/Program.cs
@@ -92,7 +92,7 @@ PrintImage(pic);
  CODE NOT WORKING*/
 
 //FACTORIAL SEARCH
-
+/*
 double Factorial(int n)
 {
 if(n==1) 
@@ -105,3 +105,30 @@ for (int i=1; i<40; i++ )
 
 {Console.WriteLine($"{i}!= {Factorial(i)}");
 }
+
+*/
+
+//числа фибоначчи
+
+//f(1)=1
+//f(2)=1
+//f(n)=f(n-1)+f(n-2)
+
+double f(int n)
+
+{
+if (n==1|n==2) return 1;
+
+else return
+f(n-1)+f(n-2);
+}
+
+for (int i=1; i<50; i++)
+{Console.WriteLine($"f({i}) = {f(i)}");
+
+}
+
+
+
+
+
